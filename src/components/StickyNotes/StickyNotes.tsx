a6cfa6ed50d36cacb1e96c06be9cca006985f70b
Eduardo Almeida Horta <eduhorta@hotmail.com>
2023-03-28 21:30:37 -0300

    before plugin

--- src/components/StickyNotes/StickyNotes.tsx
+++ src/components/StickyNotes/StickyNotes.tsx
@@ -3,16 +3,80 @@ import React from 'react';
 function StickyNotes() {
   return (
     <>
-      <div>
-        <h1>Lembretes -</h1>
+      <div className="text-center">
+        <h1 className="text-roxo-600">Lembretes -</h1>
       </div>
-      <div>
-        <h2>Título</h2>
-        <p>Descrição</p>
-      </div>
-      <div>
-        <h2>Título</h2>
-        <p>Descrição</p>
+      <div className="mx-2 flex flex-wrap justify-start gap-6">
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
+        <div className="w-48 bg-roxo-300">
+          <h2>Título</h2>
+          <p className="h-24 overflow-auto break-all">
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição Descrição Descrição Descrição
+            Descrição Descrição Descrição{' '}
+          </p>
+        </div>
       </div>
     </>
   );
