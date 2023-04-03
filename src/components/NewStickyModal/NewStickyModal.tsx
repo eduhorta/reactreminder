8d9eaeefd69ce8408d02247f769ed77f3f57a47a
Eduardo Almeida Horta <eduhorta@hotmail.com>
2023-04-03 10:19:06 -0300

    fix text en->br

--- src/components/NewStickyModal/NewStickyModal.tsx
+++ src/components/NewStickyModal/NewStickyModal.tsx
@@ -35,19 +35,19 @@ function NewStickyModal({ onClose }: NewStickyModalProps) {
               className="mb-2 block font-bold text-gray-700"
               htmlFor="note"
             >
-              Title
+              Título
             </label>
             <input
               className="border font-bold"
               type="text"
-              placeholder="Title"
+              placeholder="Título"
               onChange={(event) => setStickyTitle(event.target.value)}
               name="title"
             />
           </div>
           <div className="px-4 py-5 sm:p-6">
             <div className="mb-4">
-              <label className="mb-2 block font-bold text-gray-700">Note</label>
+              <label className="mb-2 block font-bold text-gray-700">Nota</label>
               <textarea
                 className="focus:shadow-outline w-full resize-none rounded-md border py-2 px-3 leading-tight text-gray-700 focus:outline-none"
                 rows={3}
@@ -60,14 +60,14 @@ function NewStickyModal({ onClose }: NewStickyModalProps) {
                 type="submit"
                 className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white focus:outline-none hover:bg-blue-700"
               >
-                Save
+                Salvar
               </button>
               <button
                 type="button"
                 className="focus:shadow-outline ml-4 rounded bg-gray-500 py-2 px-4 font-bold text-white focus:outline-none hover:bg-gray-700"
                 onClick={onClose}
               >
-                Cancel
+                Cancelar
               </button>
             </div>
           </div>
